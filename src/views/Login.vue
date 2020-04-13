2b3ebc2063dbeaba6892878a39feb0445b3f1a0d
varunkumarmedam <varunkumarmedam@gmail.com>
2020-04-14 02:07:20 +0530

    fixing google btn

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
+  <div class="col-12">
+    <div class="col-8 mx-auto">
+      <img src="../assets/login_logo.jpg" height="500px" width="100%" />
+    </div>
+    <div class="col-3 google_btn">
+      <a href="#" class="google">
+        <img src="https://img.icons8.com/color/16/000000/google-logo.png" /> Signup Using Google
+      </a>
+    </div>
     <a href="https://biheros.herokuapp.com/google">Login with Google</a>
     <br />
     <router-link to="/set">Lets Set Cookies</router-link>
   </div>
 </template>
+
+<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 
 <script>
 export default {
@@ -22,3 +32,32 @@ export default {
   }
 };
 </script>
+
+<style>
+.google_btn{
+  color: white;
+  background-color: #4285f4;
+  
+}
+a.google{
+  color: white;
+}
+.btn {
+  color: blue;
+  border-radius: 2px;
+  text-transform: capitalize;
+  font-size: 15px;
+  padding: 10px 19px;
+  cursor: pointer;
+}
+.btn-md {
+  padding: 10px 16px;
+  font-size: 15px;
+  line-height: 23px;
+}
+.btn-google {
+  color: #1300bd;
+  background-color: #ffffff;
+  box-shadow: 0 1px 2px 1px #ddd;
+}
+</style>
